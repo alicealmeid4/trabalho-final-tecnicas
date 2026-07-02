a66f9f4b7e250628f8f358df098137c4c761f67c
alicealmeida26 <alice@careintelligence.ai>
2026-07-02 18:33:29 -0300

    refactor: renomeia variáveis para nomes mais descritivos

--- Main.java
+++ Main.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class Main {
 
-	static char[] f; // fita
-	static int p; // posicao
-	static String est; //estado atual
-	static List<String[]> t = new ArrayList<String[]>();
+	static char[] fita;
+	static int posicao;
+	static String estadoAtual;
+	static List<String[]> transicoes = new ArrayList<String[]>();
 	static Set<String> finais = new HashSet<String>();
-	static int cont = 0;
+	static int contadorPassos = 0;
 
     public static void main(String[] args) {
 
 		// maquina que incrementa um numero binario em 1
-		t.add(new String[]{"q0","0","q0","0","D"});
-		t.add(new String[]{"q0","1","q0","1","D"});
-		t.add(new String[]{"q0","_","q1","_","E"});
-		t.add(new String[]{"q1","1","q1","0","E"});
-		t.add(new String[]{"q1","0","qf","1","P"});
-		t.add(new String[]{"q1","_","qf","1","P"});
+		transicoes.add(new String[]{"q0","0","q0","0","D"});
+		transicoes.add(new String[]{"q0","1","q0","1","D"});
+		transicoes.add(new String[]{"q0","_","q1","_","E"});
+		transicoes.add(new String[]{"q1","1","q1","0","E"});
+		transicoes.add(new String[]{"q1","0","qf","1","P"});
+		transicoes.add(new String[]{"q1","_","qf","1","P"});
 
 		finais.add("qf");
 
@@ -26,84 +26,84 @@ public class Main {
 			entrada=args[0];
 		}
 
-		f = new char[100];
-		for(int i=0;i<f.length;i++){
-			f[i]='_';
+		fita = new char[100];
+		for(int i=0;i<fita.length;i++){
+			fita[i]='_';
 		}
 		for(int i=0;i<entrada.length();i++){
-			f[i+40]=entrada.charAt(i);
+			fita[i+40]=entrada.charAt(i);
 		}
 
-		p=40;
-		est="q0";
+		posicao=40;
+		estadoAtual="q0";
 
 		System.out.println("Fita inicial:");
 		imprimeFita();
 
 		boolean rodando=true;
 		while(rodando){
-			cont++;
-			if(cont>1000){
+			contadorPassos++;
+			if(contadorPassos>1000){
 				System.out.println("deu ruim, muitas iteracoes");
 				break;
 			}
 
-			char simb = f[p];
-			String[] trans = null;
-			for(int i=0;i<t.size();i++){
-				String[] linha = t.get(i);
-				if(linha[0].equals(est) && linha[1].charAt(0)==simb){
-					trans=linha;
+			char simboloLido = fita[posicao];
+			String[] transicaoEncontrada = null;
+			for(int i=0;i<transicoes.size();i++){
+				String[] regra = transicoes.get(i);
+				if(regra[0].equals(estadoAtual) && regra[1].charAt(0)==simboloLido){
+					transicaoEncontrada=regra;
 					break;
 				}
 			}
 
-			if(trans==null){
-				System.out.println("nao ha transicao para estado "+est+" e simbolo "+simb);
+			if(transicaoEncontrada==null){
+				System.out.println("nao ha transicao para estado "+estadoAtual+" e simbolo "+simboloLido);
 				rodando=false;
 			}else{
 
-				f[p]=trans[3].charAt(0);
-				est=trans[2];
+				fita[posicao]=transicaoEncontrada[3].charAt(0);
+				estadoAtual=transicaoEncontrada[2];
 
-				if(trans[4].equals("D")){
-					p=p+1;
+				if(transicaoEncontrada[4].equals("D")){
+					posicao=posicao+1;
 				}
-				if(trans[4].equals("E")){
-					p=p-1;
+				if(transicaoEncontrada[4].equals("E")){
+					posicao=posicao-1;
 				}
-				if(trans[4].equals("P")){
+				if(transicaoEncontrada[4].equals("P")){
 					rodando=false;
 				}
 
-				System.out.println("passo "+cont+" -> estado="+est+" pos="+p);
+				System.out.println("passo "+contadorPassos+" -> estado="+estadoAtual+" pos="+posicao);
 				imprimeFita();
 			}
 		}
 
-		if(finais.contains(est)){
+		if(finais.contains(estadoAtual)){
 			System.out.println("ACEITA");
 		}else{
 			System.out.println("REJEITA (ou parou sem chegar em estado final)");
 		}
 
 		System.out.println("resultado final:");
-		String res="";
-		for(int i=0;i<f.length;i++){
-			if(f[i]!='_')res=res+f[i];
+		String resultado="";
+		for(int i=0;i<fita.length;i++){
+			if(fita[i]!='_')resultado=resultado+fita[i];
 		}
-		System.out.println(res);
+		System.out.println(resultado);
     }
 
 	public static void imprimeFita(){
 		String s = "";
 		for (int i = 30; i < 55; i++) {
-			s += f[i];
+			s += fita[i];
 		}
 		System.out.println(s);
 		String marcador="";
 		for(int i=30;i<55;i++){
-			if(i==p) marcador+="^"; else marcador+=" ";
+			if(i==posicao) marcador+="^"; else marcador+=" ";
 		}
 		System.out.println(marcador);
 	}
